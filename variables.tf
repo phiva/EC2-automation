38986ee8b0ed8f99992feb54d75b235fd53d3c24
phiva <phiva1985@gmail.com>
2025-06-02 18:09:20 -0300

    Update env files and region

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variable "region" {
-  description = "AWS region"
-  default     = "us-east-1"
-}
+#variable "region" {
+ # description = "AWS region"
+  # default     = "us-east-1"
+# }
 
 variable "ami_id" {
   description = "AMI ID"
